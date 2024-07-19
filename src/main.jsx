385f146057f20105e776473a8135724ec438e2a5
Charles Villalpando <charlesvill@g.ucla.edu>
2024-07-19 09:03:34 -0700

    Global .env variable for api key

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const apiKey = process.env.API_KEY;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
