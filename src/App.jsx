94f4c34fe4a031254e623d13fa231bbd5d40d387
Charles Villalpando <iroh-thinkpad@General-Iroh.attlocal.net>
2026-08-17 12:59:39 -0700

    error logging for deployed instance

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { Outlet } from 'react-router-dom'
-import { useState } from 'react';
-import NavBar from './components/navbar/navbar';
-import Footer from './components/footer/footer';
+import { Outlet } from "react-router-dom";
+import { useState } from "react";
+import NavBar from "./components/navbar/navbar";
+import Footer from "./components/footer/footer";
 import "./App.css";
 
 function App() {
   const [cart, setCart] = useState([]);
-
+  console.log("MODE:", import.meta.env.MODE);
+  console.log("API URL:", import.meta.env.VITE_API_URL);
+  console.log("ALL ENV:", import.meta.env);
   return (
     <>
       <NavBar cart={cart} />
